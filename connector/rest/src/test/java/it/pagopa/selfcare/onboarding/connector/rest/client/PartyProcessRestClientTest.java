f9e9e42bc61439d24630806592c99dcbef3162aa
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-04-08 15:09:00 +0200

    Onboarding response fix

--- connector/rest/src/test/java/it/pagopa/selfcare/onboarding/connector/rest/client/PartyProcessRestClientTest.java
+++ connector/rest/src/test/java/it/pagopa/selfcare/onboarding/connector/rest/client/PartyProcessRestClientTest.java
@@ -6,13 +6,13 @@ import it.pagopa.selfcare.commons.connector.rest.RestTestUtils;
 import it.pagopa.selfcare.commons.utils.TestUtils;
 import it.pagopa.selfcare.onboarding.connector.rest.config.PartyProcessRestClientTestConfig;
 import it.pagopa.selfcare.onboarding.connector.rest.model.OnboardingRequest;
-import it.pagopa.selfcare.onboarding.connector.rest.model.OnboardingResponse;
 import it.pagopa.selfcare.onboarding.connector.rest.model.User;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.commons.httpclient.HttpClientConfiguration;
 import org.springframework.context.ApplicationContextInitializer;
@@ -64,8 +64,6 @@ class PartyProcessRestClientTest extends BaseFeignRestClientTest {
 
     private static final Map<TestCase, String> testCase2instIdMap = new EnumMap<>(TestCase.class) {{
         put(TestCase.FULLY_VALUED, "institutionId1");
-        put(TestCase.FULLY_NULL, "institutionId2");
-        put(TestCase.EMPTY_RESULT, "institutionId3");
     }};
 
     @Autowired
@@ -73,32 +71,17 @@ class PartyProcessRestClientTest extends BaseFeignRestClientTest {
 
 
     @Test
-    void onboardingOrganization_fullyValued() {
+    void onboardingOrganization() {
         // given
         OnboardingRequest onboardingRequest = new OnboardingRequest();
         onboardingRequest.setInstitutionId(testCase2instIdMap.get(TestCase.FULLY_VALUED));
         onboardingRequest.setUsers(List.of(TestUtils.mockInstance(new User())));
         // when
-        OnboardingResponse response = restClient.onboardingOrganization(onboardingRequest);
+        Executable exe = () -> restClient.onboardingOrganization(onboardingRequest);
         // then
-        Assertions.assertNotNull(response);
-        Assertions.assertNotNull(response.getToken());
-        Assertions.assertNotNull(response.getDocument());
+        Assertions.assertDoesNotThrow(exe);
     }
 
 
-    @Test
-    void onboardingOrganization_fullyNull() {
-        // given
-        OnboardingRequest onboardingRequest = new OnboardingRequest();
-        onboardingRequest.setInstitutionId(testCase2instIdMap.get(TestCase.FULLY_NULL));
-        onboardingRequest.setUsers(List.of(TestUtils.mockInstance(new User())));
-        // when
-        OnboardingResponse response = restClient.onboardingOrganization(onboardingRequest);
-        // then
-        Assertions.assertNotNull(response);
-        Assertions.assertNull(response.getToken());
-        Assertions.assertNull(response.getDocument());
-    }
 
 }
