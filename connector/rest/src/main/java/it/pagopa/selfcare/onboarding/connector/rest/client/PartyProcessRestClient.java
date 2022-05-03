1937497ab3c4dc39bcae3a3375d4f26754bd25c1
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-05-03 15:13:22 +0200

    institutionId refactoring

--- connector/rest/src/main/java/it/pagopa/selfcare/onboarding/connector/rest/client/PartyProcessRestClient.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/onboarding/connector/rest/client/PartyProcessRestClient.java
@@ -38,7 +38,7 @@ public interface PartyProcessRestClient {
     @GetMapping(value = "${rest-client.party-process.getOnBoardingInfo.path}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     @CollectionFormat(feign.CollectionFormat.CSV)
-    OnBoardingInfo getOnBoardingInfo(@RequestParam(value = "institutionId", required = false) String institutionId,
+    OnBoardingInfo getOnBoardingInfo(@RequestParam(value = "externalInstitutionId", required = false) String externalInstitutionId,
                                      @RequestParam(value = "states", required = false) EnumSet<RelationshipState> states);
 
     @GetMapping(value = "${rest-client.party-process.getInstitutionByExternalId.path}", produces = MediaType.APPLICATION_JSON_VALUE)
