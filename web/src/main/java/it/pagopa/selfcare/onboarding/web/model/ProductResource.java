b311234a01b362121a31dfe4af88b2e0a95b4062
VALUETEAM\MannaSa <salvatore11.manna@emea.nttdata.com>
2022-11-29 16:56:18 +0100

    Modified API to retrieve a product in the onboarding phase, the API now also displays the "status" field

--- web/src/main/java/it/pagopa/selfcare/onboarding/web/model/ProductResource.java
+++ web/src/main/java/it/pagopa/selfcare/onboarding/web/model/ProductResource.java
@@ -2,6 +2,7 @@ package it.pagopa.selfcare.onboarding.web.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
+import it.pagopa.selfcare.onboarding.connector.model.product.ProductStatus;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -21,4 +22,9 @@ public class ProductResource {
 
     @ApiModelProperty(value = "${swagger.onboarding.product.model.parentId}", required = false)
     private String parentId;
+
+    @ApiModelProperty(value = "${swagger.onboarding.product.model.status}", required = true)
+    @JsonProperty(required = true)
+    @NotBlank
+    private ProductStatus status;
 }
