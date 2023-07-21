c77233d490b4eec3f90c3a2ba063025e44d0834a
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2023-07-21 16:06:02 +0200

    SELC-2716 add FeignErrorDecoder Test case

--- connector-api/src/main/java/it/pagopa/selfcare/onboarding/connector/exceptions/InternalGatewayErrorException.java
+++ connector-api/src/main/java/it/pagopa/selfcare/onboarding/connector/exceptions/InternalGatewayErrorException.java
@@ -3,8 +3,4 @@ package it.pagopa.selfcare.onboarding.connector.exceptions;
 public class InternalGatewayErrorException extends RuntimeException {
     public InternalGatewayErrorException() {
     }
-
-    public InternalGatewayErrorException(String message) {
-        super(message);
-    }
 }
