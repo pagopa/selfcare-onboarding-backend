cde1bd7ab1c8694a7aed64a81988d17ceb046fb2
manuraf <manuel.rafeli@pagopa.it>
2023-08-31 16:25:50 +0200

    fix: added check if taxCode is blank @/institutions/geographicTaxonomies

--- connector-api/src/main/java/it/pagopa/selfcare/onboarding/connector/exceptions/InvalidRequestException.java
+++ connector-api/src/main/java/it/pagopa/selfcare/onboarding/connector/exceptions/InvalidRequestException.java
@@ -2,4 +2,7 @@ package it.pagopa.selfcare.onboarding.connector.exceptions;
 
 public class InvalidRequestException extends RuntimeException {
     public InvalidRequestException() {}
+    public InvalidRequestException(String message) {
+        super(message);
+    }
 }
