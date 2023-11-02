60d5c20b8ab39176032664b4b0ed676a8cae38a1
kevin.si1396 <kevin.si1396@gmail.com>
2023-11-02 11:02:19 +0100

    add institution location informations to onboardingDto model

---
+++ connector-api/src/main/java/it/pagopa/selfcare/onboarding/connector/model/onboarding/InstitutionLocation.java
@@ -0,0 +1,10 @@
+package it.pagopa.selfcare.onboarding.connector.model.onboarding;
+
+import lombok.Data;
+
+@Data
+public class InstitutionLocation {
+    private String city;
+    private String county;
+    private String country;
+}
