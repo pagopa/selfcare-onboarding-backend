47f4aa3531fb9c26f6da68e7911ccdfc5ee35c45
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2024-01-17 09:29:05 +0100

    feat: added new AdditionalInformations Object

---
+++ web/src/main/java/it/pagopa/selfcare/onboarding/web/model/AdditionalInformationsDto.java
@@ -0,0 +1,36 @@
+package it.pagopa.selfcare.onboarding.web.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class AdditionalInformationsDto {
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.belongRegulatedMarket}")
+    private boolean belongRegulatedMarket;
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.regulatedMarketNote}")
+    private String regulatedMarketNote;
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.isIpa}")
+    private boolean isIpa;
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.ipaCode}")
+    private String ipaCode;
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.establishedByRegulatoryProvision}")
+    private boolean establishedByRegulatoryProvision;
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.establishedByRegulatoryProvisionNote}")
+    private String establishedByRegulatoryProvisionNote;
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.isAgentOfPublicService}")
+    private boolean isAgentOfPublicService;
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.agentOfPublicServiceNote}")
+    private boolean agentOfPublicServiceNote;
+
+    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.assistance.otherNote}")
+    private String otherNote;
+
+}
