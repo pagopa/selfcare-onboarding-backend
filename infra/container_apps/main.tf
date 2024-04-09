7da14cd5223373466e6c5a62ec5a24f41e7e762f
manuraf <manuel.rafeli@pagopa.it>
2024-04-09 16:06:54 +0200

    container_app_environment_name

--- infra/container_apps/main.tf
+++ infra/container_apps/main.tf
@@ -16,6 +16,7 @@ module "container_app_onboarding_backend" {
   env_short          = var.env_short
   container_app      = var.container_app
   container_app_name = "onboardingbackend"
+  container_app_environment_name = local.container_app_environment_name
   image_name         = "selfcare-onboarding-backend"
   image_tag          = var.image_tag
   app_settings       = var.app_settings
