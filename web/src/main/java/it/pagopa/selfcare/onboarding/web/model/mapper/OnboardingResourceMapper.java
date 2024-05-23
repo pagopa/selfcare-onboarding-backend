7d206498c9ed14849e6c33ad235d360732606b85
andrea-putzu <andrea.putzu@pagopa.it>
2024-05-23 16:03:29 +0200

    feat: Added originId to Onboarding model when starting an onboarding request

--- web/src/main/java/it/pagopa/selfcare/onboarding/web/model/mapper/OnboardingResourceMapper.java
+++ web/src/main/java/it/pagopa/selfcare/onboarding/web/model/mapper/OnboardingResourceMapper.java
@@ -28,7 +28,7 @@ public interface OnboardingResourceMapper {
     @Mapping(source = "assistanceContacts.supportEmail", target = "institutionUpdate.supportEmail")
     @Mapping(source = "assistanceContacts.supportPhone", target = "institutionUpdate.supportPhone")
     @Mapping(source = "additionalInformations", target = "institutionUpdate.additionalInformations")
-    @Mapping(source = "ivassCode", target = "originId")
+    @Mapping(source = "originId", target = "originId")
     OnboardingData toEntity(OnboardingProductDto dto);
 
     @Mapping(source = "billingData", target = "billing")
