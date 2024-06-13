602b730ce2b27ddc205e510a3be466cb4e5f7248
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-06-13 09:21:00 +0200

    Removed issue reported by Sonar

--- connector/rest/src/test/java/it/pagopa/selfcare/onboarding/connector/OnboardingMsConnectorImplTest.java
+++ connector/rest/src/test/java/it/pagopa/selfcare/onboarding/connector/OnboardingMsConnectorImplTest.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class OnboardingMsConnectorImplTest {
+class OnboardingMsConnectorImplTest {
 
     @InjectMocks
     private OnboardingMsConnectorImpl onboardingMsConnector;
