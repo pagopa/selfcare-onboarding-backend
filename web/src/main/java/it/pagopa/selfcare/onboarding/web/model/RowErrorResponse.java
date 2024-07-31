05156557da4e16a13f1dc2608804d2778a3bcbf8
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-07-31 18:52:49 +0200

    [SELC-5359] Added json property in RowErrorResponse

--- web/src/main/java/it/pagopa/selfcare/onboarding/web/model/RowErrorResponse.java
+++ web/src/main/java/it/pagopa/selfcare/onboarding/web/model/RowErrorResponse.java
@@ -1,10 +1,17 @@
 package it.pagopa.selfcare.onboarding.web.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class RowErrorResponse {
+
+    @JsonProperty("riga")
     private Integer row;
+
+    @JsonProperty("codice fiscale")
     private String cf;
+
+    @JsonProperty("errore")
     private String reason;
 }
