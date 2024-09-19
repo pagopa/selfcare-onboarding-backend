9db7ecb0f21d4ea39378705c7e716d246930d279
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2024-09-19 12:20:10 +0200

    fix: fixed checkManager

--- web/src/main/java/it/pagopa/selfcare/onboarding/web/controller/UserController.java
+++ web/src/main/java/it/pagopa/selfcare/onboarding/web/controller/UserController.java
@@ -16,10 +16,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Collections;
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
 
@@ -78,14 +79,14 @@ public class UserController {
                     @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE,
                             schema = @Schema(implementation = Problem.class))
             })
-    @PostMapping(value = "/check-manager", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/check-manager")
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation(value = "", notes = "${swagger.onboarding.users.api.check-manager}", nickname = "checkManager")
-    public ResponseEntity<Boolean> checkManager(@RequestBody @Valid OnboardingUserDto request) {
+    public Map<String, Boolean> checkManager(@RequestBody @Valid OnboardingUserDto request) {
         log.trace("onboarding start");
-        boolean checkManager = userService.checkManager(onboardingResourceMapper.toEntity(request));
+        boolean checkManager =  userService.checkManager(onboardingResourceMapper.toEntity(request));
         log.trace("onboarding end");
-        return ResponseEntity.ok(checkManager);
+        return Collections.singletonMap("result", checkManager);
     }
 
 }
