7473011b12aae5a36a55c2afc1d7e21e4c3bb68d
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2025-03-10 16:40:38 +0100

    Removed sonar issues

--- connector/rest/src/main/java/it/pagopa/selfcare/onboarding/connector/rest/client/MsOnboardingInstitutionApiClient.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/onboarding/connector/rest/client/MsOnboardingInstitutionApiClient.java
@@ -1,7 +1,6 @@
 package it.pagopa.selfcare.onboarding.connector.rest.client;
 
 import it.pagopa.selfcare.onboarding.generated.openapi.v1.api.InstitutionControllerApi;
-import it.pagopa.selfcare.onboarding.generated.openapi.v1.api.OnboardingControllerApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "${rest-client.ms-onboarding-institution-api.serviceCode}", url = "${rest-client.ms-onboarding.base-url}")
