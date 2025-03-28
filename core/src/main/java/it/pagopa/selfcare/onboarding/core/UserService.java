da749310d7bd02aa2a85864e2b968cc43b0b7d53
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-03-28 12:23:53 +0100

    Added method into UserService for verify user

--- core/src/main/java/it/pagopa/selfcare/onboarding/core/UserService.java
+++ core/src/main/java/it/pagopa/selfcare/onboarding/core/UserService.java
@@ -4,9 +4,15 @@ import it.pagopa.selfcare.onboarding.connector.model.onboarding.OnboardingData;
 import it.pagopa.selfcare.onboarding.connector.model.onboarding.User;
 
 public interface UserService {
-    void validate(User user);
-    void onboardingUsers(OnboardingData onboardingData);
-    void onboardingUsersAggregator(OnboardingData onboardingData);
-    boolean checkManager(OnboardingData onboardingData);
-    User getManagerInfo(String onboardingId, String userTaxCode);
+  void validate(User user);
+
+  void onboardingUsers(OnboardingData onboardingData);
+
+  void onboardingUsersAggregator(OnboardingData onboardingData);
+
+  boolean checkManager(OnboardingData onboardingData);
+
+  User getManagerInfo(String onboardingId, String userTaxCode);
+
+  boolean isAllowedUserByUid(String uid);
 }
