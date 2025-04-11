e263d5e8926d86dc64319132b04dcc93d827bf0a
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-04-11 12:44:46 +0200

    Added connector to user institution

---
+++ connector/rest/src/main/java/it/pagopa/selfcare/onboarding/connector/rest/config/MsUserInstitutionApiClientConfig.java
@@ -0,0 +1,15 @@
+package it.pagopa.selfcare.onboarding.connector.rest.config;
+
+import it.pagopa.selfcare.commons.connector.rest.config.RestClientBaseConfig;
+import it.pagopa.selfcare.onboarding.connector.rest.client.*;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Import(RestClientBaseConfig.class)
+@EnableFeignClients(clients = {MsUserInstitutionApiClient.class})
+@PropertySource("classpath:config/ms-user-institution-rest-client.properties")
+public class MsUserInstitutionApiClientConfig {
+}
