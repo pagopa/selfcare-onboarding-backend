500ee35569a23e369faf643336f554a977bcffef
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-07-04 12:42:48 +0200

    Added mock data

--- app/src/test/resources/db/init_mongo.js
+++ app/src/test/resources/db/init_mongo.js
@@ -1,53 +1,190 @@
 db = db.getSiblingDB("selcOnboarding");
 
-db.onboardings.insertOne({
-  createdAt: new Date(),
-});
-
-db.onboardings.insertOne({
-  _id: "89ad7142-24bb-48ad-8504-9c9231137i103",
-  createdAt: ISODate("2024-10-18T12:24:50.050Z"),
-  institution: {
-    address: "sede leg",
-    city: "Milano",
-    country: "IT",
-    county: "MI",
-    description: "Techpartner 23-02",
-    digitalAddress: "pec@pectest.com",
-    geographicTaxonomies: [],
-    id: "c2808e95-59a7-44fb-a6df-64faf4ff3ed3",
-    imported: false,
-    institutionType: "PT",
-    origin: "INFOCAMERE",
-    taxCode: "11223345665",
-    zipCode: "11223"
+db.onboardings.insertMany([
+  {
+    _id: "89ad7142-24bb-48ad-8504-9c9231137i103",
+    createdAt: ISODate("2024-10-18T12:24:50.050Z"),
+    institution: {
+      address: "sede leg",
+      city: "Milano",
+      country: "IT",
+      county: "MI",
+      description: "Techpartner 23-02",
+      digitalAddress: "pec@pectest.com",
+      geographicTaxonomies: [],
+      id: "c2808e95-59a7-44fb-a6df-64faf4ff3ed3",
+      imported: false,
+      institutionType: "PT",
+      origin: "INFOCAMERE",
+      taxCode: "11223345665",
+      zipCode: "11223",
+    },
+    productId: "prod-io",
+    status: "PENDING",
+    users: [
+      {
+        id: "ec7ca4d5-c537-462a-8c06-e102e0c21c44",
+        role: "MANAGER",
+        userMailUuid: "ID_MAIL#49e16aa5-bc28-40c7-9844-6886e4647e22",
+      },
+    ],
+    workflowType: "USERS",
   },
-  productId: "prod-io",
-  status: "PENDING",
-  users: [
-    {
-      id: "ec7ca4d5-c537-462a-8c06-e102e0c21c44",
-      role: "MANAGER",
-      userMailUuid: "ID_MAIL#49e16aa5-bc28-40c7-9844-6886e4647e22"
-    }
-  ],
-  workflowType: "USERS_PG"
-});
-
+  {
+    _id: "89ad7142-24bb-48ad-8504-9c9231137i105",
+    createdAt: ISODate("2024-10-18T12:24:50.050Z"),
+    institution: {
+      address: "sede leg",
+      city: "Milano",
+      country: "IT",
+      county: "MI",
+      description: "Techpartner 23-02",
+      digitalAddress: "pec@pectest.com",
+      geographicTaxonomies: [],
+      id: "c2808e95-59a7-44fb-a6df-64faf4ff3ed3",
+      imported: false,
+      institutionType: "PT",
+      origin: "INFOCAMERE",
+      taxCode: "11223345665",
+      zipCode: "11223",
+    },
+    productId: "prod-io",
+    status: "PENDING",
+    users: [
+      {
+        id: "ec7ca4d5-c537-462a-8c06-e102e0c21c44",
+        role: "MANAGER",
+        userMailUuid: "ID_MAIL#49e16aa5-bc28-40c7-9844-6886e4647e22",
+      },
+    ],
+    workflowType: "USERS",
+    referenceOnboardingId: "89ad7142-24bb-48ad-8504-9c9231137i110",
+  },
+  {
+    _id: "89ad7142-24bb-48ad-8504-9c9231137i110",
+    createdAt: ISODate("2024-10-18T12:24:50.050Z"),
+    institution: {
+      address: "sede leg",
+      city: "Milano",
+      country: "IT",
+      county: "MI",
+      description: "Techpartner 23-02",
+      digitalAddress: "pec@pectest.com",
+      geographicTaxonomies: [],
+      id: "c2808e95-59a7-44fb-a6df-64faf4ff3ed3",
+      imported: false,
+      institutionType: "PT",
+      origin: "INFOCAMERE",
+      taxCode: "11223345667",
+      zipCode: "11223",
+    },
+    productId: "prod-io",
+    status: "COMPLETED",
+    users: [
+      {
+        id: "ec7ca4d5-c537-462a-8c06-e102e0c21c44",
+        role: "MANAGER",
+        userMailUuid: "ID_MAIL#49e16aa5-bc28-40c7-9844-6886e4647e22",
+      },
+    ],
+    workflowType: "CONTRACT_REGISTRATION",
+  },
+  {
+    _id: "89ad7142-24bb-48ad-8504-9c9231137i122",
+    createdAt: ISODate("2024-10-18T12:24:50.050Z"),
+    institution: {
+      address: "sede leg",
+      city: "Milano",
+      country: "IT",
+      county: "MI",
+      description: "Techpartner 23-02",
+      digitalAddress: "pec@pectest.com",
+      geographicTaxonomies: [],
+      id: "c2808e95-59a7-44fb-a6df-64faf4ff3ed3",
+      imported: false,
+      institutionType: "PT",
+      origin: "INFOCAMERE",
+      taxCode: "11223345661",
+      zipCode: "11223",
+    },
+    productId: "prod-io",
+    status: "COMPLETED",
+    users: [
+      {
+        id: "ec7ca4d5-c537-462a-8c06-e102e0c21c44",
+        role: "MANAGER",
+        userMailUuid: "ID_MAIL#49e16aa5-bc28-40c7-9844-6886e4647e22",
+      },
+    ],
+    workflowType: "CONTRACT_REGISTRATION",
+  },
+  {
+    _id: "89ad7142-24bb-48ad-8504-9c9231137i1022",
+    createdAt: ISODate("2024-10-18T12:24:50.050Z"),
+    institution: {
+      address: "sede leg",
+      city: "Milano",
+      country: "IT",
+      county: "MI",
+      description: "Techpartner 23-02",
+      digitalAddress: "pec@pectest.com",
+      geographicTaxonomies: [],
+      id: "c2808e95-59a7-44fb-a6df-64faf4ff3ed3",
+      imported: false,
+      institutionType: "PT",
+      origin: "INFOCAMERE",
+      taxCode: "11223345661",
+      zipCode: "11223",
+    },
+    productId: "prod-io",
+    status: "TOBEVALIDATED",
+    users: [
+      {
+        id: "ec7ca4d5-c537-462a-8c06-e102e0c21c44",
+        role: "MANAGER",
+        userMailUuid: "ID_MAIL#49e16aa5-bc28-40c7-9844-6886e4647e22",
+      },
+    ],
+    workflowType: "FOR_APPROVE",
+  }
+]);
 
 db.tokens.insertOne({
   createdAt: new Date(),
 });
 
-db.tokens.insertOne({
-  _id: "89ad7142-24bb-48ad-8504-9c9231137i103",
-  onboardingId: "89ad7142-24bb-48ad-8504-9c9231137i103",
-  type: "INSTITUTION",
-  productId: "prod-io",
-  contractTemplate: "contracts/template/io/2.4.2/io-accordo_di_adesione-v.2.4.2.html",
-  createdAt: new ISODate("2021-10-20T12:53:34.000Z"),
-  updatedAt: new ISODate("2023-07-25T10:24:08.119Z")
-});
+db.tokens.insertMany([
+  {
+    _id: "89ad7142-24bb-48ad-8504-9c9231137i103",
+    onboardingId: "89ad7142-24bb-48ad-8504-9c9231137i103",
+    type: "INSTITUTION",
+    productId: "prod-io",
+    contractTemplate:
+      "contracts/template/io/2.4.2/io-accordo_di_adesione-v.2.4.2.html",
+    createdAt: new ISODate("2021-10-20T12:53:34.000Z"),
+    updatedAt: new ISODate("2023-07-25T10:24:08.119Z"),
+  },
+  {
+    _id: "89ad7142-24bb-48ad-8504-9c9231137i105",
+    onboardingId: "89ad7142-24bb-48ad-8504-9c9231137i105",
+    type: "INSTITUTION",
+    productId: "prod-io",
+    contractTemplate:
+      "contracts/template/io/2.4.2/io-accordo_di_adesione-v.2.4.2.html",
+    createdAt: new ISODate("2021-10-20T12:53:34.000Z"),
+    updatedAt: new ISODate("2023-07-25T10:24:08.119Z"),
+  },
+  {
+    _id: "89ad7142-24bb-48ad-8504-9c9231137i122",
+    onboardingId: "89ad7142-24bb-48ad-8504-9c9231137i122",
+    type: "INSTITUTION",
+    productId: "prod-io",
+    contractTemplate:
+      "contracts/template/io/2.4.2/io-accordo_di_adesione-v.2.4.2.html",
+    createdAt: new ISODate("2021-10-20T12:53:34.000Z"),
+    updatedAt: new ISODate("2023-07-25T10:24:08.119Z"),
+  },
+]);
 
 db = db.getSiblingDB("selcMsCore");
 
--- app/src/test/resources/features/token.feature
+++ app/src/test/resources/features/token.feature
@@ -12,4 +12,58 @@ Feature: Token
     When I send a POST request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i104/complete" with multi-part file
     Then The status code is 400
     And The response body contains:
-      | errors[0].detail | Onboarding with id 89ad7142-24bb-48ad-8504-9c9231137i104 not found or it is expired!|
+      | errors[0].detail | Onboarding with id 89ad7142-24bb-48ad-8504-9c9231137i104 not found or it is expired! |
+
+  Scenario: Success to complete onboarding-users
+    Given User login with username "j.doe" and password "test"
+    And A mock-file of type "application/pdf" with key "contract" and document path "mock/mock-template.pdf" used to perform request
+    When I send a POST request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i105/complete-onboarding-users" with multi-part file
+    Then The status code is 204
+
+  Scenario: Failed to complete onboarding-users when onboarding is missing
+    Given User login with username "j.doe" and password "test"
+    And A mock-file of type "application/pdf" with key "contract" and document path "mock/mock-template.pdf" used to perform request
+    When I send a POST request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i106/complete-onboarding-users" with multi-part file
+    Then The status code is 400
+    And The response body contains:
+      | errors[0].detail | Onboarding with id 89ad7142-24bb-48ad-8504-9c9231137i106 not found or it is expired! |
+
+  Scenario: Success to verify onboarding
+    Given User login with username "j.doe" and password "test"
+    When I send a POST request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i103/verify"
+    Then The status code is 200
+
+  Scenario: Failed to verify onboarding when not found
+    Given User login with username "j.doe" and password "test"
+    When I send a POST request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i106/verify"
+    Then The status code is 404
+
+  Scenario: Success to retrive onboarding
+    Given User login with username "j.doe" and password "test"
+    When I send a GET request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i122"
+    Then The status code is 204
+
+  Scenario: Failed to retrive onboarding
+    Given User login with username "j.doe" and password "test"
+    When I send a GET request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i1019"
+    Then The status code is 404
+
+  Scenario: Success to approve onboarding
+    Given User login with username "j.doe" and password "test"
+    When I send a GET request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i1022/approve"
+    Then The status code is 200
+
+  Scenario: Failed to approve onboarding when given invalid id
+    Given User login with username "j.doe" and password "test"
+    When I send a GET request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i101/approve"
+    Then The status code is 400
+
+  Scenario: Failed to approve onboarding when is not found
+    Given User login with username "j.doe" and password "test"
+    When I send a GET request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i101/approve"
+    Then The status code is 404
+
+  Scenario: Failed to approve onboarding when is already consumed
+    Given User login with username "j.doe" and password "test"
+    When I send a GET request to "/v2/tokens/89ad7142-24bb-48ad-8504-9c9231137i105/approve"
+    Then The status code is 409
