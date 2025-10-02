86b58bac94fc770f83778e9954610660d5d4dbeb
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-10-02 17:10:51 +0200

    Updated ref of lib institutionType

--- connector-api/src/main/java/it/pagopa/selfcare/onboarding/connector/model/institutions/InstitutionInfo.java
+++ connector-api/src/main/java/it/pagopa/selfcare/onboarding/connector/model/institutions/InstitutionInfo.java
@@ -1,7 +1,7 @@
 package it.pagopa.selfcare.onboarding.connector.model.institutions;
 
 import it.pagopa.selfcare.commons.base.security.PartyRole;
-import it.pagopa.selfcare.commons.base.utils.InstitutionType;
+import it.pagopa.selfcare.onboarding.common.InstitutionType;
 import it.pagopa.selfcare.onboarding.connector.model.onboarding.*;
 import lombok.Data;
 
