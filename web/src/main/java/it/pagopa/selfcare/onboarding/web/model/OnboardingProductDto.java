7585626fd2e6355372a465132895e2ec91a741fb
pierpaolo.didato@emeal.nttdata.com <pierpaolo.didato@emeal.nttdata.com>
2025-11-12 14:52:04 +0100

    Updates

--- web/src/main/java/it/pagopa/selfcare/onboarding/web/model/OnboardingProductDto.java
+++ web/src/main/java/it/pagopa/selfcare/onboarding/web/model/OnboardingProductDto.java
@@ -88,8 +88,4 @@ public class OnboardingProductDto {
     @ApiModelProperty(value = "${swagger.onboarding.institutions.model.payment}")
     private PaymentDto payment;
 
-    @ApiModelProperty(value = "${swagger.onboarding.institutions.model.toAddOnAggregates}")
-    private Boolean toAddOnAggregates;
-
-
 }
